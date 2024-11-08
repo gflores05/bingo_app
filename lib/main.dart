4a02aed7cd27fb84a09afd8d77a71992f4ce312e
gflores05 <gabriel.flores0503@gmail.com>
2024-11-08 15:23:49 -0600

    feat(raffle): generate random number

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         // This is the theme of your application.
         primarySwatch: Palette.red,
       ),
-      home: BingoNewGame(),
+      home: const BingoRaffle(),
       routes: {
         BingoNewGame.routeName: (ctx) => BingoNewGame(),
         BingoTable.routeName: (ctx) => const BingoTable(),
